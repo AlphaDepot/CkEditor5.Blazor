84e1ab344fdb2ce865fee9bdb3f416d7ad719797
lstsystems <admin@lstsystems.com>
2025-07-09 17:09:13 -0400

    Updated CKEditor to major version 46.0.0 and upgraded all other Node dependencies to their latest versions.

--- CkEditor5.Blazor/wwwroot/js/src/vite.config.ts
+++ CkEditor5.Blazor/wwwroot/js/src/vite.config.ts
@@ -20,28 +20,3 @@ export default defineConfig({
         minify: false
     }
 })
-
-/*
-export default defineConfig({
-    build: {
-        outDir: path.resolve(__dirname, '../dist'),
-        lib: {
-            entry: path.resolve(__dirname, 'lib/main.ts'),
-            name: 'ckeditor5-blazor',
-            fileName: () => 'main.js'
-        },
-        rollupOptions: {
-            output: {
-                entryFileNames: '[name].js',
-                chunkFileNames: '[name].js',
-                assetFileNames: '[name].[ext]',
-                manualChunks(id) {
-                    if (id.includes('main.ts')) {
-                        return 'main';
-                    }
-                },
-                inlineDynamicImports: false
-            }
-        }
-    }
-})*/
